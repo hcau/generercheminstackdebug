5c07d6f800375691d450d2ebdf89228d6e0470c4
Herve Caujolle <herve.caujolle@gmail.com>
2026-09-19 08:40:53 +0200

    [EXO] Ajout d'un commentaire sysout.

--- src/fr/exemple/pdf/PdfDemo.java
+++ src/fr/exemple/pdf/PdfDemo.java
@@ -6,6 +6,9 @@ public class PdfDemo {
 
     public static void main(String[] args) {
 
+        System.out.println("programme principal, main, HCau");
+        
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=== Écran ===");

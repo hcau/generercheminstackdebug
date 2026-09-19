7a78f959e82868c69273fae841de6e359e091835
Herve Caujolle <herve.caujolle@gmail.com>
2026-09-19 12:33:04 +0200

    Correction sans prendre en compte le dev remote

--- src/exo/synchronize/Calculatrice.java
+++ src/exo/synchronize/Calculatrice.java
@@ -3,8 +3,7 @@ package exo.synchronize;
 public class Calculatrice {
 
     public int addition(int a, int b) {
-<<<<<<< HEAD
         System.out.println("Addition");
-        return a + b +1200;
+        return a + b + 400/8;
     }
 }

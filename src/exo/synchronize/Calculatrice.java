994380e3fe63827a62ffbd945fcae1ae277c29f7
Herve Caujolle <herve.caujolle@gmail.com>
2026-09-20 11:42:57 +0200

    Correction

--- src/exo/synchronize/Calculatrice.java
+++ src/exo/synchronize/Calculatrice.java
@@ -3,7 +3,7 @@ package exo.synchronize;
 public class Calculatrice {
 
     public int addition(int a, int b) {
-        System.out.println("Additionjjjj");
+        System.out.println("Addition");
         return a + b + 50;
     }
 }
